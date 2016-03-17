57095da9ef2cd38e620d4ae7f97dc095cbe27d2a
黃書妍 <smile14097@yahoo.com.tw>
2016-03-17 21:46:55 +0800

    hello~~~

---
+++ hw1.js
@@ -0,0 +1,36 @@
+function minus() {
+  if (document.getElementById("num")) {
+    var curr = parseInt(document.getElementById("num").innerHTML);
+  }
+
+  if (curr && curr <= 100) {
+
+    if (curr <= 10) {
+      document.getElementById("num").innerHTML = "0" + (curr - 1);
+    } else {
+      document.getElementById("num").innerHTML = curr - 1;
+    }
+  } else {
+    document.getElementById("num").innerHTML = 60;
+  }
+
+  setTimeout(function() {
+    minus();
+  }, 1000);
+};
+minus();
+
+
+var card = document.getElementById('card');
+var clicked = false;
+
+card.onclick = function(){
+  if(clicked == false) {
+    this.classList.add('hover');
+    clicked = true;
+  }
+  else {
+    this.classList.remove('hover');
+    clicked = false;
+  }
+}
